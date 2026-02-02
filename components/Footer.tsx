0a1747caf05457b2eb61c00d240ccf3a8f596e52
Momen <mmoham14@msudenver.edu>
2026-02-01 18:15:22 -0700

    Working App Structure

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>© {new Date().getFullYear()} My Electronic Portfolio. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+
+function Header(){
+    return (
+        <header>
+            <h1 className="title">React App Header</h1>
+            <nav className="navBox">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
